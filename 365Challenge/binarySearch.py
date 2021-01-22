54dee861afe1c08920a34a3ea211226e87f4fdad
gandastik <gandastik@gmail.com>
2021-01-22 19:48:33 +0700

    Day22

---
+++ 365Challenge/binarySearch.py
@@ -0,0 +1,20 @@
+#Jan 22, 2021 - binary search with the sorted list, return out the index if there is no element return -1.
+from typing import List
+def bSearch(lst: List[int], n: int, lo: int, hi: int) -> int:
+    mid = int((lo+hi) / 2)
+    if(lst[mid] == n):
+        return mid
+    elif(lst[mid] > n):
+        hi = mid
+        return bSearch(lst, n, lo, hi)
+    elif(lst[mid] < n):
+        lo = mid
+        return bSearch(lst, n, lo, hi)
+    elif(mid == lo) : return -1
+def binarySearch(lst: List[int], n: int) -> int:
+    return bSearch(lst, n, 0, len(lst))
+
+lst = [int(x) for x in input().split()]
+lst.sort()
+n = int(input())
+print(binarySearch(lst, n))
