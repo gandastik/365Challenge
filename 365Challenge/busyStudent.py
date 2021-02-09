0d871ea03fae4678d87d4ad06227b55c555c75d5
gandastik <gandastik@gmail.com>
2021-02-09 19:12:08 +0700

    Day40

--- 365Challenge/busyStudent.py
+++ 365Challenge/busyStudent.py
@@ -4,11 +4,11 @@
 
 from typing import List
 def busyStudent(startTime: List[int], endTime: List[int], queryTime: int) -> int:
-        count = 0
-        for i in range(len(startTime)):
-            if(queryTime <= endTime[i] and queryTime >= startTime[i]):
-                count+=1
-        return count
+    count = 0
+    for i in range(len(startTime)):
+        if(queryTime <= endTime[i] and queryTime >= startTime[i]):
+            count+=1
+    return count
 
 startTime = [int(x) for x in input().split()]
 endTime = [int(x) for x in input().split()]
