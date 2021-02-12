507cb633024170805e57c9d32029060c1dba91c5
gandastik <gandastik@gmail.com>
2021-02-12 11:45:41 +0700

    Day43

---
+++ 365Challenge/subtractProductAndSum.py
@@ -0,0 +1,14 @@
+#43. Feb 12, 2021 - Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+def subtractProductAndSum(n: int) -> int:
+    temp = n
+    Sum = 0
+    product = 1
+    while(temp > 0):
+        r = temp % 10
+        Sum += r
+        product *= r
+        temp = temp // 10
+    return product - Sum 
+
+n = int(input())
+print(subtractProductAndSum(n))
